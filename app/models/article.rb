f58ab5fd0e5f376d4390754b4e5f68a4b062beb3
Rohan Baidya <baidya.rohan@gmail.com>
2022-06-23 10:26:21 +0545

    Blog Complete

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 10 }
+  validates :body, presence: true, length: { minimum: 10 }
+
+end
